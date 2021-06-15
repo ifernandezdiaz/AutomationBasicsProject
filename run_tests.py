da38076948c8e7b603ac28b9185cb26952941c07
ifernandezdiaz <ifernandez.qa@gmail.com>
2021-06-15 13:36:28 -0300

    Adding .gitignore + latests changes to reporter

--- run_tests.py
+++ run_tests.py
@@ -6,9 +6,9 @@ import HtmlTestRunner
 # Método que nos permite listar todos los archivos de tests
 test_suite = unittest.defaultTestLoader.discover(".")
 
-# Definimos el nombre del reporte
+# Definimos la carpeta del reporte (si la misma existe, no la vuelve a crear)
 report_path = "./reports/"
-
+os.makedirs(report_path, exist_ok=True)
 
 # Abrimos el reporte y le cargamos los resultados
 runner = HtmlTestRunner.HTMLTestRunner(output=report_path,report_name="automation_report",
